54662e1017d61efc58a33a343709ef7c2e2fe207
Feifei HANG <l.daniel.hung@gmail.com>
2014-11-20 12:59:26 +0000

    stylish look and feel:sparkles::sparkles::sparkles:

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,6 +96,7 @@ module.exports = function (grunt) {
           {expand: true, src: ['./overlay-template.html'], dest: 'dist/'},
           {expand: true, src: ['./remove-template.html'], dest: 'dist/'},
           {expand: true, src: ['./img/markdown.png'], dest: 'dist/'},
+          {expand: true, src: ['./img/blueprint.png'], dest: 'dist/'},
           {expand: true, src: ['./php/sync.php'], dest: 'dist/'}
         ]
       }
